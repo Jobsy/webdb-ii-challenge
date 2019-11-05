2b4eeae8c62ed8d3497f7f5d770dcf85f4f1572b
Oluwajoba <oluwajobabello@gmail.com>
2019-11-05 16:10:55 +0100

    update knexfile config settings / knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,18 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './car-dealer.db3'
+    },
+    useNullAsDefault: true
   },
 
-  staging: {
-    client: 'postgresql',
+  
+  production:  {
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './car-dealer-production.db3'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true
   }
 
 };
