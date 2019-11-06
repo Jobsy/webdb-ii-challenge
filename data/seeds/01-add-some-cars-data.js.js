bf028b7df1a12ca7b422d6b968b9dc70ee23b94b
Oluwajoba <oluwajobabello@gmail.com>
2019-11-06 05:33:03 +0100

    run knex with seeds / seeds/01-add-some-cars-data.js

--- data/seeds/01-add-some-cars-data.js.js
+++ data/seeds/01-add-some-cars-data.js.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex("cars").truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('cars').insert([
+        {VIN: "12345", make:"toyota", model:"corola", mileage: "8888", "transmission-type": "manual", status: "good" },
+        {VIN: "67890", make:"honda", model:"accord", mileage: "7777", "transmission-type": "automatic", status: "" },
+        {VIN: "01234", make:"toyota", model:"camry", mileage: "5555", "transmission-type": "automatic", status: "ok" }
       ]);
     });
 };
